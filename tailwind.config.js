40f46631e095215febde4f33ea82fce6e8a22e17
Praveen Perera <praveen@avencera.com>
2023-08-18 15:59:59 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./easywind_lib/templates/**/*.{j2, html}"],
+  content: ["./easywind/templates/**/*.{j2, html}"],
   theme: {
     extend: {},
   },
