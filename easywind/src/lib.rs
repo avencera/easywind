989697f60c6214ab27e39e90ca56549fc72fcffb
Praveen Perera <praveen@avencera.com>
2023-08-19 16:27:46 -0400

    Remove all code add assets and install script to new branch

--- easywind/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod consts;
-pub mod init;
-pub mod server;
-pub mod start;
-pub mod tailwind;
-pub mod template;
-pub mod validate;
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./easywind/templates/**/*.{j2, html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
